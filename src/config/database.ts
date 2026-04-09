cedd23dce8c0db06744e68310d712d39dca50f00
BladimirGS <hugobladimirgs@gmail.com>
2026-04-08 19:35:54 -0600

    Correccion path sequelize-typescript

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,7 +13,7 @@ export const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   logging: false, 
-  models: [path.join(__dirname, '../models/**/*.ts')], 
+  models: [path.join(__dirname, '../models/**/*.{js,ts}')],
   define: {
     timestamps: false, 
   },
